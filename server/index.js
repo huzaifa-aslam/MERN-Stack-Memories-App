b260b06684cf49b8a3224344d570800162ee4ee9
Huzaifa Aslam <huzaifaslam45@gmail.com>
2021-03-28 01:29:25 +0500

    added heroku setup

--- server/index.js
+++ server/index.js
@@ -3,17 +3,20 @@ import mongoose from "mongoose";
 import cors from "cors";
 import bodyParser from "body-parser";
 import postRoutes from "./routes/post.js";
+import dotenv from "dotenv";
 const app = express();
-
+dotenv.config()
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes)
+app.get('/', (req, res) => {
+  res.send('Hello to Memories App')
+})
 
-const CONNECTION_URL =
-  "mongodb+srv://admin:admin123@cluster0.74wh8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose
