deae68167528acfb4c230409a7193781b9ce9cf6
Huzaifa Aslam <huzaifaslam45@gmail.com>
2021-03-28 14:19:15 +0500

    minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,48 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import memories from "./images/memories.png";
 import Form from "./components/Form/Form";
 import Posts from "./components/Posts/Posts";
-import  useStyles from "./styles";
+import useStyles from "./styles";
 const App = () => {
-    const [currentPostId, setCurrentPostId] = useState(null)
-    const classes = useStyles();
+  const [currentPostId, setCurrentPostId] = useState(null);
+  const classes = useStyles();
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading}  variant="h2" align="center">
+        <Typography className={classes.heading} variant="h2" align="center">
           Memories
         </Typography>
-        <img className={classes.image} src={memories} alt="memories" height="60" />
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
       </AppBar>
       <Grow in>
-        <Grid
-          container
-          justify="space-between"
-          alignItems="stretch"
-          spacing={3}
-        >
-          <Grid item xs={12} sm={7}>
-              <Posts currentPostId={currentPostId} setCurrentPostId={setCurrentPostId}/>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+            className={classes.mainContaier}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts
+                currentPostId={currentPostId}
+                setCurrentPostId={setCurrentPostId}
+              />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form
+                currentPostId={currentPostId}
+                setCurrentPostId={setCurrentPostId}
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={4}>
-              <Form currentPostId={currentPostId} setCurrentPostId={setCurrentPostId}/>
-          </Grid>
-        </Grid>
+        </Container>
       </Grow>
     </Container>
   );
